89daf32de10950a105b15436c92b959c66985515
Facundo Pugliese <fpugliese09@gmail.com>
2023-08-28 22:03:53 -0300

    Minor fix

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -7,14 +7,6 @@ import { client } from '@/sanity/lib/client';
 import PreviewProvider from '@/app/_components/preview.provider';
 import PreviewPost from '@/app/_components/preview.post';
 
-// Prepare Next.js to know which routes already exist
-export async function generateStaticParams() {
-  // Important, use the plain Sanity Client here
-  const posts = await client.fetch(postPathsQuery);
-
-  return posts;
-}
-
 export default async function Page({ params }: { params: any }) {
   const post = await sanityFetch<SanityDocument>({ query: postQuery, params });
   const isDraftMode = draftMode().isEnabled;
