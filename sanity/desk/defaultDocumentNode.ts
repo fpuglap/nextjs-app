e40c40968a7baa324ef316df09bb273f038a42bb
Facundo Pugliese <fpugliese09@gmail.com>
2023-09-02 03:56:13 -0300

    Minor fix

--- sanity/desk/defaultDocumentNode.ts
+++ sanity/desk/defaultDocumentNode.ts
@@ -1,13 +1,5 @@
 import { DefaultDocumentNodeResolver } from 'sanity/desk';
 import Iframe from 'sanity-plugin-iframe-pane';
-import { SanityDocument } from 'sanity';
-
-// Customise this function to show the correct URL based on the current document
-// function getPreviewUrl(doc: SanityDocument) {
-//   return doc?.slug?.current
-//     ? `${window.location.host}/${doc.slug.current}`
-//     : `${window.location.host}`;
-// }
 
 function getPreviewUrl() {
   return process.env.NODE_ENV === 'development'
